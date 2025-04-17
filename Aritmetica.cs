4aa508c87376759278b08e077c439cbed64123f2
João Bessa <joaofrbessa@gmail.com>
2025-04-17 19:27:38 +0100

    Adicionar comentários XML aos métodos Somar() e Subtrair()

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -3,15 +3,26 @@ namespace Calculos{
 //Implmenta operações aritméticas
 internal class Aritmetica{
 
+    /// <summary>
+    /// Operação Soma.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns>Retirna a soma de dois números</returns>
     public int Somar(int x, int y)
     {
         return x + y;
     }
-
+/// <summary>
+/// Operação Subtrair
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+/// <returns>Retorna o resultado da subtração de dois números</returns>
     public int Subtrar(int x, int y)
     {
 
-            return x - y;
+        return x - y;
 
     }
 
